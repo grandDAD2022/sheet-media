83ba1a8b013b44a96f0a03092f9e4899e9c90d4b
Xerz <xerz@xerz.one>
2022-04-17 21:19:08 +0200

    Commit inicial

---
+++ src/main/java/com/github/grandDAD2022/sheetmedia/SheetMediaApplication.java
@@ -0,0 +1,13 @@
+package com.github.grandDAD2022.sheetmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/grandDAD2022/sheetmedia/SheetMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grandDAD2022.sheetmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
