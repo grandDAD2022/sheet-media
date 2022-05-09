6b8bc1532e5caef3c2f08af587b740872dfbf3a0
RubBen19 <98461533+RubBen19@users.noreply.github.com>
2022-05-10 01:15:41 +0200

    Limpieza

--- src/main/java/com/github/grandDAD2022/sheet/media/db/MediaRepository.java
+++ src/main/java/com/github/grandDAD2022/sheet/media/db/MediaRepository.java
@@ -1,18 +1,7 @@
 package com.github.grandDAD2022.sheet.media.db;
 
-import java.util.List;
-
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@CacheConfig(cacheNames = "fotos")
 public interface MediaRepository extends JpaRepository<Media, Long>{
-	
-	@CacheEvict(allEntries=true)
-	Media save(Media media);
-	
-	@Cacheable
-	List<Media> findAll();
+
 }
